97251e61e99d84c60700a39e21318c5bca10db6d
nikitsya <nikitasmiichyk@gmail.com>
2026-05-02 01:52:02 +0100

    Add JSDoc for map page initialisation

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import {stadiums} from "./js/data/locations.js"
 import {MapManager} from "./js/map/MapManager.js"
 
+/**
+ * Initialises the Google Map and connects city buttons to stadium markers.
+ */
 export function initialiseMapPage() {
     const cityButtons = document.querySelectorAll(".ns_cityButton")
     const mapManager = new MapManager(stadiums)
 
     mapManager.init()
 
-    // Focus the map on the selected city's stadium
     cityButtons.forEach(button => {
         button.addEventListener("click", () => {
             const cityIndex = Number(button.value)
